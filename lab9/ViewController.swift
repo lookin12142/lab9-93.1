6c9fc03524c63c4a34077b576a1b71e70079478a
lucas <lucas.ponce@tecsup.edu.pe>
2024-05-06 16:54:20 -0500

    Initial Commit

---
+++ lab9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab9
+//
+//  Created by lucas on 6/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
